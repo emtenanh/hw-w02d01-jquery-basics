57e7f36dd6e8a74467666d856fe5549a85747976
Emtenan Hejji <emtenan@MacBook-Pro-alkhas-b-Emtenan.local>
2019-04-14 23:18:52 +0300

    submit jquery hw

--- index.js
+++ index.js
@@ -2,23 +2,52 @@
 function turnOnStopLight() {
     // change background color of stop light to red
     // changes background color of green and yellow to black
+    turnOffLights();
+    $("#stopLight").css("background-color","red");
+    
 }
-
+$("#stopButton").on("click",turnOnStopLight);
 // turn on slow function
 function turnOnSlowLight() { 
   // change background color of slow light to yellow
   // changes background color of red and green to black
+  turnOffLights();
+  $("#slowLight").css("background-color","yellow");
+    
 }
+$("#slowButton").on("click",turnOnSlowLight);
 
 // turn on go function
 function turnOnGoLight() {
     // change background color of go light to green
     // changes background color of red and yellow to black
-}
+    turnOffLights();
+    $("#goLight").css("background-color","green");
+    
+  }
+  $("#goButton").on("click",turnOnGoLight);
 
 // set stop light click event
 
 // set slow light click event
 
 // set go light click event
+
+// turnOffLights(light) {
+
+// }
+
+function turnOffLights(){
+  $("#goLight").css("background-color","black");
+  $("#slowLight").css("background-color","black");
+  $("#stopLight").css("background-color","black");
+}
+
+
+
+// function turnOffLights($(this)){
+
+// }
+
+
 
